1f3ddf1ff1bee35cc3d881f38fa226a8c019d9bb
royriojas <roy@kno.com>
2015-08-14 19:20:18 -0700

    FEAT: Add a convenience method to expand globs

---
+++ specs/lib/expand-globs.js
@@ -0,0 +1,83 @@
+describe( 'expand-globs', function () {
+  //var proxyquire = require('proxyquire');
+  it( 'should expand globs and ignore the given ones', function () {
+
+    var expandGlobs = require( '../../lib/expand-globs' );
+
+    var files = expandGlobs( [
+      'resources/expand/**/*.*',
+      '!resources/expand/**/*.txt'
+    ] );
+    var path = require( 'path' );
+
+    var expected = [
+      'resources/expand/demo-1.js',
+      'resources/expand/demo-1.less',
+      'resources/expand/demo-2.js',
+      'resources/expand/demo-2.less',
+      'resources/expand/demo-3.js',
+      'resources/expand/demo-3.less',
+      //'resources/expand/sub-folder/sub-sub-1/demo-1.txt',
+      'resources/expand/sub-folder/sub-sub-1/demo-2.css',
+      'resources/expand/sub-folder/sub-sub-1/demo-3.jpg'
+    ].map( function ( file ) {
+      return path.resolve( process.cwd(), file );
+    } );
+
+    expect( files ).to.deep.equal( expected );
+  } );
+
+  it( 'should expand globs and ignore the given ones even if the they have different signatures to refer to relative files', function () {
+
+    var expandGlobs = require( '../../lib/expand-globs' );
+
+    var files = expandGlobs( [
+      './resources/expand/**/*.*',
+      '!resources/expand/**/*.txt'
+    ] );
+    var path = require( 'path' );
+
+    var expected = [
+      'resources/expand/demo-1.js',
+      'resources/expand/demo-1.less',
+      'resources/expand/demo-2.js',
+      'resources/expand/demo-2.less',
+      'resources/expand/demo-3.js',
+      'resources/expand/demo-3.less',
+      //'resources/expand/sub-folder/sub-sub-1/demo-1.txt',
+      'resources/expand/sub-folder/sub-sub-1/demo-2.css',
+      'resources/expand/sub-folder/sub-sub-1/demo-3.jpg'
+    ].map( function ( file ) {
+      return path.resolve( process.cwd(), file );
+    } );
+
+    expect( files ).to.deep.equal( expected );
+  } );
+
+  it( 'should expand globs and ignore the given ones even if the they have different signatures to refer to relative files', function () {
+
+    var expandGlobs = require( '../../lib/expand-globs' );
+
+    var files = expandGlobs( [
+      'resources/expand/**/*.*',
+      '!./resources/expand/**/*.txt'
+    ] );
+    var path = require( 'path' );
+
+    var expected = [
+      'resources/expand/demo-1.js',
+      'resources/expand/demo-1.less',
+      'resources/expand/demo-2.js',
+      'resources/expand/demo-2.less',
+      'resources/expand/demo-3.js',
+      'resources/expand/demo-3.less',
+      //'resources/expand/sub-folder/sub-sub-1/demo-1.txt',
+      'resources/expand/sub-folder/sub-sub-1/demo-2.css',
+      'resources/expand/sub-folder/sub-sub-1/demo-3.jpg'
+    ].map( function ( file ) {
+      return path.resolve( process.cwd(), file );
+    } );
+
+    expect( files ).to.deep.equal( expected );
+  } );
+} );
