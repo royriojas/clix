7c052d83dcd387be93aa997f1413afdcedb3b257
royriojas <roy@kno.com>
2015-08-15 03:07:57 -0700

    FEAT: Implement expandMapping similar to the one in grunt

--- specs/lib/expand-mapping.js
+++ specs/lib/expand-mapping.js
@@ -8,26 +8,24 @@ describe( 'expand-mapping', function () {
         dest: 'dest/'
       } );
 
-      var expected = {
-        'files': [
-          {
-            'src': [
-              './resources/expand/demo-1.js',
-              './resources/expand/demo-1.less',
-              './resources/expand/demo-2.js',
-              './resources/expand/demo-2.less',
-              './resources/expand/demo-3.js',
-              './resources/expand/demo-3.less',
-              './resources/expand/sub-folder',
-              './resources/expand/sub-folder/sub-sub-1',
-              './resources/expand/sub-folder/sub-sub-1/demo-1.txt',
-              './resources/expand/sub-folder/sub-sub-1/demo-2.css',
-              './resources/expand/sub-folder/sub-sub-1/demo-3.jpg'
-            ],
-            'dest': 'dest/'
-          }
-        ]
-      };
+      var expected = [
+        {
+          'src': [
+            './resources/expand/demo-1.js',
+            './resources/expand/demo-1.less',
+            './resources/expand/demo-2.js',
+            './resources/expand/demo-2.less',
+            './resources/expand/demo-3.js',
+            './resources/expand/demo-3.less',
+            './resources/expand/sub-folder',
+            './resources/expand/sub-folder/sub-sub-1',
+            './resources/expand/sub-folder/sub-sub-1/demo-1.txt',
+            './resources/expand/sub-folder/sub-sub-1/demo-2.css',
+            './resources/expand/sub-folder/sub-sub-1/demo-3.jpg'
+          ],
+          'dest': './dest/'
+        }
+      ];
 
       expect( mappings ).to.deep.equal( expected );
     } );
