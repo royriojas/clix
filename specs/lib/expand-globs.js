8ba8b30889c5443da05e179a7c56e55a3fb71104
royriojas <roy@kno.com>
2015-08-14 23:16:29 -0700

    REF: do not resolve paths by default
    If the full paths are needed specify the option `resolvePaths: true`
    
    ```javascript
    var files = cli.expandGlobs(globs, { resolvePaths: true });
    ```

--- specs/lib/expand-globs.js
+++ specs/lib/expand-globs.js
@@ -7,7 +7,7 @@ describe( 'expand-globs', function () {
     var files = expandGlobs( [
       'resources/expand/**/*.*',
       '!resources/expand/**/*.txt'
-    ] );
+    ], { resolvePaths: true } );
     var path = require( 'path' );
 
     var expected = [
@@ -34,7 +34,7 @@ describe( 'expand-globs', function () {
     var files = expandGlobs( [
       './resources/expand/**/*.*',
       '!resources/expand/**/*.txt'
-    ] );
+    ], { resolvePaths: true } );
     var path = require( 'path' );
 
     var expected = [
@@ -54,14 +54,15 @@ describe( 'expand-globs', function () {
     expect( files ).to.deep.equal( expected );
   } );
 
-  it( 'should expand globs and ignore the given ones even if the they have different signatures to refer to relative files', function () {
+  it( 'should expand globs and ignore the given ones even if the they have different signatures to refer to relative files, inverted', function () {
 
     var expandGlobs = require( '../../lib/expand-globs' );
 
     var files = expandGlobs( [
       'resources/expand/**/*.*',
       '!./resources/expand/**/*.txt'
-    ] );
+    ], { resolvePaths: true } );
+
     var path = require( 'path' );
 
     var expected = [
@@ -79,5 +80,81 @@ describe( 'expand-globs', function () {
     } );
 
     expect( files ).to.deep.equal( expected );
+  } );
+
+  describe( 'no resolve', function () {
+    it( 'should expand globs and ignore the given ones', function () {
+
+      var expandGlobs = require( '../../lib/expand-globs' );
+
+      var files = expandGlobs( [
+        'resources/expand/**/*.*',
+        '!resources/expand/**/*.txt'
+      ] );
+
+      //var path = require( 'path' );
+
+      var expected = [
+        './resources/expand/demo-1.js',
+        './resources/expand/demo-1.less',
+        './resources/expand/demo-2.js',
+        './resources/expand/demo-2.less',
+        './resources/expand/demo-3.js',
+        './resources/expand/demo-3.less',
+        //'./resources/expand/sub-folder/sub-sub-1/demo-1.txt',
+        './resources/expand/sub-folder/sub-sub-1/demo-2.css',
+        './resources/expand/sub-folder/sub-sub-1/demo-3.jpg'
+      ];
+
+      expect( files ).to.deep.equal( expected );
+    } );
+
+    it( 'should expand globs and ignore the given ones even if the they have different signatures to refer to relative files', function () {
+
+      var expandGlobs = require( '../../lib/expand-globs' );
+
+      var files = expandGlobs( [
+        './resources/expand/**/*.*',
+        '!resources/expand/**/*.txt'
+      ] );
+
+      var expected = [
+        './resources/expand/demo-1.js',
+        './resources/expand/demo-1.less',
+        './resources/expand/demo-2.js',
+        './resources/expand/demo-2.less',
+        './resources/expand/demo-3.js',
+        './resources/expand/demo-3.less',
+        //'./resources/expand/sub-folder/sub-sub-1/demo-1.txt',
+        './resources/expand/sub-folder/sub-sub-1/demo-2.css',
+        './resources/expand/sub-folder/sub-sub-1/demo-3.jpg'
+      ];
+
+      expect( files ).to.deep.equal( expected );
+    } );
+
+    it( 'should expand globs and ignore the given ones even if the they have different signatures to refer to relative files, inverted', function () {
+
+      var expandGlobs = require( '../../lib/expand-globs' );
+
+      var files = expandGlobs( [
+        'resources/expand/**/*.*',
+        '!./resources/expand/**/*.txt'
+      ] );
+
+      var expected = [
+        './resources/expand/demo-1.js',
+        './resources/expand/demo-1.less',
+        './resources/expand/demo-2.js',
+        './resources/expand/demo-2.less',
+        './resources/expand/demo-3.js',
+        './resources/expand/demo-3.less',
+        //'./resources/expand/sub-folder/sub-sub-1/demo-1.txt',
+        './resources/expand/sub-folder/sub-sub-1/demo-2.css',
+        './resources/expand/sub-folder/sub-sub-1/demo-3.jpg'
+      ];
+
+      expect( files ).to.deep.equal( expected );
+    } );
   } );
 } );
