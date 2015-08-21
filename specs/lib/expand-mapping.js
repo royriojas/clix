7e810d862b23a4ef418e79fb2a365636bbb9ccc2
Roy Riojas <royriojas@gmail.com>
2015-08-21 01:37:53 -0700

    FEAT: add `ext` and `rename` options similar to grunt.file.expandMapping ones

--- specs/lib/expand-mapping.js
+++ specs/lib/expand-mapping.js
@@ -30,4 +30,229 @@ describe( 'expand-mapping', function () {
       expect( mappings ).to.deep.equal( expected );
     } );
   } );
+
+  describe( 'when using expand set to true', function () {
+    it( 'should create groups of objects with src and dest', function () {
+      var expandMapping = require( '../../lib/expand-mapping' );
+
+      var mappings = expandMapping( {
+        src: 'resources/expand/**/*.less',
+        dest: 'dest/',
+        expand: true
+      } );
+
+      var expected = [
+        {
+          'src': [
+            './resources/expand/demo-1.less'
+          ],
+          'dest': './dest/resources/expand/demo-1.less'
+        },
+        {
+          'src': [
+            './resources/expand/demo-2.less'
+          ],
+          'dest': './dest/resources/expand/demo-2.less'
+        },
+        {
+          'src': [
+            './resources/expand/demo-3.less'
+          ],
+          'dest': './dest/resources/expand/demo-3.less'
+        }
+      ];
+
+      //console.log(JSON.stringify( mappings, null, 2));
+      expect( mappings ).to.deep.equal( expected );
+    } );
+
+    describe( 'when using cwd', function () {
+      it( 'should remove the cwd part from the matched file', function () {
+        var expandMapping = require( '../../lib/expand-mapping' );
+
+        var mappings = expandMapping( {
+          src: '**/*.less',
+          dest: 'dest/css/',
+          cwd: 'resources/expand',
+          expand: true
+        } );
+
+        var expected = [
+          {
+            'src': [
+              './resources/expand/demo-1.less'
+            ],
+            'dest': './dest/css/demo-1.less'
+          },
+          {
+            'src': [
+              './resources/expand/demo-2.less'
+            ],
+            'dest': './dest/css/demo-2.less'
+          },
+          {
+            'src': [
+              './resources/expand/demo-3.less'
+            ],
+            'dest': './dest/css/demo-3.less'
+          }
+        ];
+
+        //console.log( JSON.stringify( mappings, null, 2 ) );
+        expect( mappings ).to.deep.equal( expected );
+      } );
+    } );
+
+    describe( 'when using ext', function () {
+      it( 'should replace the extension on each destination', function () {
+        var expandMapping = require( '../../lib/expand-mapping' );
+
+        var mappings = expandMapping( {
+          src: '**/*.less',
+          dest: 'dest/css/',
+          cwd: 'resources/expand',
+          ext: '.css',
+          expand: true
+        } );
+
+        var expected = [
+          {
+            'src': [
+              './resources/expand/demo-1.less'
+            ],
+            'dest': './dest/css/demo-1.css'
+          },
+          {
+            'src': [
+              './resources/expand/demo-2.less'
+            ],
+            'dest': './dest/css/demo-2.css'
+          },
+          {
+            'src': [
+              './resources/expand/demo-3.less'
+            ],
+            'dest': './dest/css/demo-3.css'
+          }
+        ];
+
+        //console.log( JSON.stringify( mappings, null, 2 ) );
+        expect( mappings ).to.deep.equal( expected );
+      } );
+    } );
+
+    describe( 'when using rename', function () {
+      it( 'should replace the dest with the value returned from the data.rename fn', function () {
+        var expandMapping = require( '../../lib/expand-mapping' );
+        var path = require( 'path' );
+
+        var mappings = expandMapping( {
+          src: '**/*.less',
+          dest: 'dest/',
+          cwd: 'resources/expand',
+          ext: '.css',
+          rename: function ( dest, file ) {
+            return './' + path.join( dest, 'css/', file );
+          },
+          expand: true
+        } );
+
+        var expected = [
+          {
+            'src': [
+              './resources/expand/demo-1.less'
+            ],
+            'dest': './dest/css/demo-1.css'
+          },
+          {
+            'src': [
+              './resources/expand/demo-2.less'
+            ],
+            'dest': './dest/css/demo-2.css'
+          },
+          {
+            'src': [
+              './resources/expand/demo-3.less'
+            ],
+            'dest': './dest/css/demo-3.css'
+          }
+        ];
+
+        expect( mappings ).to.deep.equal( expected );
+      } );
+    } );
+
+    it( 'should use the default value if no return is provided from rename', function () {
+      var expandMapping = require( '../../lib/expand-mapping' );
+
+      var mappings = expandMapping( {
+        src: '**/*.less',
+        dest: 'dest/',
+        cwd: 'resources/expand',
+        ext: '.css',
+        rename: function () {},
+        expand: true
+      } );
+
+      var expected = [
+        {
+          'src': [
+            './resources/expand/demo-1.less'
+          ],
+          'dest': './dest/demo-1.css'
+        },
+        {
+          'src': [
+            './resources/expand/demo-2.less'
+          ],
+          'dest': './dest/demo-2.css'
+        },
+        {
+          'src': [
+            './resources/expand/demo-3.less'
+          ],
+          'dest': './dest/demo-3.css'
+        }
+      ];
+
+      expect( mappings ).to.deep.equal( expected );
+    } );
+
+    it( 'should use the matched name as dest if no destination is provided', function () {
+      var expandMapping = require( '../../lib/expand-mapping' );
+
+      var mappings = expandMapping( {
+        src: '**/*.less',
+        //dest: 'dest/',
+        cwd: 'resources/expand',
+        ext: '.css',
+        rename: function () {},
+        expand: true
+      } );
+
+      var expected = [
+        {
+          'src': [
+            './resources/expand/demo-1.less'
+          ],
+          'dest': './demo-1.css'
+        },
+        {
+          'src': [
+            './resources/expand/demo-2.less'
+          ],
+          'dest': './demo-2.css'
+        },
+        {
+          'src': [
+            './resources/expand/demo-3.less'
+          ],
+          'dest': './demo-3.css'
+        }
+      ];
+
+      expect( mappings ).to.deep.equal( expected );
+    } );
+
+  } );
 } );
